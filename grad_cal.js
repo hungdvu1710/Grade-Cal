608b115cc1b77cb1f9030b9f0a266738811dab3f
hungdvu1710 <hungvsme@gmail.com>
2020-05-21 10:14:41 +0700

    Update localStorage
    create local storage and fix small coding styles

--- grad_cal.js
+++ grad_cal.js
@@ -1,11 +1,20 @@
-let studentID = 0;
+//let studentID = localStorage.length;
+studentID = localStorage.length;
+for(let i = 1; i<=localStorage.length;i++)
+{
+    console.log(localStorage[i]);
+    arr = localStorage.getItem(`${i}`).split(",")
+    console.log(arr)
+    appendRow(`${i}`,arr[0],arr[1],arr[2],arr[3]);
+}
 document.querySelector("#submit").addEventListener("click",submit);
 document.getElementById("analyze").style.visibility = "hidden";
 document.getElementById("calculate").style.visibility = "hidden";
 const inputScore = document.getElementsByClassName("inputScore");
+const inputName = document.querySelector(".fullname");
 document.getElementById("calculate").addEventListener("click",calculate);
 document.getElementById("analyze").addEventListener("click",analyze);
-const invalidChars = [
+const invalidCharsInScore = [
   "-",
   "+",
   "e",
@@ -13,11 +22,20 @@ const invalidChars = [
 ];
 for(const i of inputScore){
     i.addEventListener("keydown", function(e) {
-        if (invalidChars.includes(e.key)) {
+        if (invalidCharsInScore.includes(e.key)) {
           e.preventDefault();
         }
     })
 }
+const invalidCharsInName = [
+    ";",
+    ",",
+];
+inputName.addEventListener("keydown", function(e) {
+    if (invalidCharsInName.includes(e.key)) {
+    e.preventDefault();
+    }
+});
 function setDefaultInputClass(){
     document.querySelector("#fullname").className = "fullname";
     document.querySelector("#math").className = "inputScore";
@@ -48,27 +66,39 @@ function appendRow(studentID, fullname, math, phy, chem) {
     //add new row to the end of database
     const tableRef = document.querySelector("#data");
     const newRow = tableRef.insertRow(-1);
-    const cell1 = newRow.insertCell(0);
+    //const cell1 = newRow.insertCell(0);
+    const cell1 = document.createElement("td");
     cell1.innerText = studentID;
+    newRow.appendChild(cell1);
 
-    const cell2 = newRow.insertCell(1);
+    //const cell2 = newRow.insertCell(1);
+    const cell2 = document.createElement("td");
     cell2.innerText = fullname;
+    newRow.appendChild(cell2);
 
-    const cell3 = newRow.insertCell(2);
+    //const cell3 = newRow.insertCell(2);
+    const cell3 = document.createElement("td");
     cell3.innerText = math;
     cell3.className = "score";
+    newRow.appendChild(cell3);
 
-    const cell4 = newRow.insertCell(3);
+    //const cell4 = newRow.insertCell(3);
+    const cell4 = document.createElement("td");
     cell4.className = "score";
     cell4.innerText = phy;
+    newRow.appendChild(cell4);
 
-    const cell5 = newRow.insertCell(4);
+    //const cell5 = newRow.insertCell(4);
+    const cell5 = document.createElement("td");
     cell5.className = "score";
     cell5.innerText = chem;
+    newRow.appendChild(cell5);
 
-    const cell6 = newRow.insertCell(5);
+    //const cell6 = newRow.insertCell(5);
+    const cell6 = document.createElement("td");
     cell6.innerText = "?";
     cell6.className = "mean";
+    newRow.appendChild(cell6);
 }
 
 function submit(){
@@ -76,9 +106,9 @@ function submit(){
     const math = Number(document.querySelector("#math").value);
     const phy = Number(document.querySelector("#phy").value);
     const chem = Number(document.querySelector("#chem").value);
-    studentID++;
     if(isNameValid(fullname) && isScoreValid(math) && isScoreValid(phy) && isScoreValid(math) && isScoreValid(chem)){
         console.info("success")
+        studentID++;
         appendRow(studentID,fullname,math,phy,chem);
         setDefaultInputClass()
         document.querySelector("#fullname").value = "";
@@ -110,7 +140,9 @@ function getMean(row){
 function calculate(){
     rows =  document.querySelectorAll("#data tr:not(:first-child)");
     for(let row of rows){
-        row.cells.item(5).innerText = getMean(row);
+        const val = row.cells;
+        val.item(5).innerText = getMean(row);
+        localStorage.setItem(val.item(0).innerText,`${val.item(1).innerText},${val.item(2).innerText},${val.item(3).innerText},${val.item(4).innerText},${val.item(5).innerText}`);
     }
     document.getElementById("analyze").style.visibility = "visible";
     document.getElementById("calculate").style.visibility = "hidden";
@@ -126,3 +158,4 @@ function analyze(){
     }
     document.getElementById("analyze").style.visibility = "hidden";
 }
+//localStorage.clear();
